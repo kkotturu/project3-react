d8c60264b07ba41c54454b0656c633d4be59b83a
Kiran <kkotturu@gmail.com>
2019-06-01 15:11:52 -0400

    Heroku might work, then again, it might not

---
+++ client/build/precache-manifest.3296f8a4410fe0e03c173397a10acd61.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "e0b05ad411ca75474995",
+    "url": "/static/css/main.4928b3f5.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b238d392af5ba4ea8c6febe37cf3625a",
+    "url": "/static/media/DogBrews.b238d392.jpg"
+  },
+  {
+    "revision": "bfa59498b0c633a6e9ab",
+    "url": "/static/js/2.7b1452ab.chunk.js"
+  },
+  {
+    "revision": "0d733fcacd7a92389c8600d14b0a6c3d",
+    "url": "/static/media/Logo.0d733fca.png"
+  },
+  {
+    "revision": "c5590569aab553af03722baa7d379978",
+    "url": "/static/media/loading.c5590569.svg"
+  },
+  {
+    "revision": "e0b05ad411ca75474995",
+    "url": "/static/js/main.dceae32d.chunk.js"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "bfa59498b0c633a6e9ab",
+    "url": "/static/css/2.b7ed9254.chunk.css"
+  },
+  {
+    "revision": "2122ec06c9d6c59a7d13d47ec8efe984",
+    "url": "/index.html"
+  }
+];
