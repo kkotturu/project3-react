b42f1b0c12030e7e6934c4e078cfadcb94bca158
Kiran <kkotturu@gmail.com>
2019-06-01 15:33:16 -0400

    Modified for convenience and safety

--- config/config.js
+++ config/config.js
@@ -1,4 +1,7 @@
-{
+require("dotenv").config();
+
+
+module.exports = {
   "development": {
     "username": "root",
     "password": "password",
@@ -14,10 +17,6 @@
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "use_env_var": process.env.JAWSDB_URL
   }
 }
